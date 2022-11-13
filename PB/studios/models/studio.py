00f0b779dcf64491cb00d419fab9a8fafeafe77e
yitaoli97 <yitaoli97@gmail.com>
2022-11-12 20:39:09 -0500

    added models directory in studios app and added Studio, fitnessClass, and Amenity models

---
+++ PB/studios/models/studio.py
@@ -0,0 +1,13 @@
+from django.db import models
+
+
+class Studio(models.Model):
+    name = models.TextField()
+    address = models.TextField()
+    location = models.TextField()
+    postalCode = models.TextField()
+    phoneNumber = models.TextField()
+    images = models.TextField()
+
+    def __str__(self):
+        return f'Studio {self.name} located at {self.address}, {self.location}.'
