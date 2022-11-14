f15e02cb9c4508690ae2980c038415826bd846ec
yitaoli97 <yitaoli97@gmail.com>
2022-11-14 11:58:27 -0500

    added serializers for studio and fitnessClass

--- PB/studios/models/studio.py
+++ PB/studios/models/studio.py
@@ -1,12 +1,9 @@
 from django.db import models
-<<<<<<< HEAD
 from PB.studios.models.fitnessClass import FitnessClass
+from django.utils import timezone
+from PB.studios.serializers import 
 
 # TODO: implement images correctly
-=======
-
-
->>>>>>> 00f0b779dcf64491cb00d419fab9a8fafeafe77e
 class Studio(models.Model):
     name = models.TextField()
     address = models.TextField()
@@ -15,29 +12,16 @@ class Studio(models.Model):
     phoneNumber = models.TextField()
     images = models.TextField()
 
-<<<<<<< HEAD
 
 
     def __str__(self):
         return f'Studio {self.name} located at {self.address}, {self.location}.'
 
 
+    # implement get_info in view
 
-    def get_info(self):
-        return {
-            'name': self.name, 
-            'address': self.address, 
-            'location': self.location,
-            'postalCode': self.postalCode,
-            'phoneNumber': self.phoneNumber,
-            'images': self.images,
-        }
-
-
-    # TODO
+    
     def create_schedule(self):
-        pass
-=======
-    def __str__(self):
-        return f'Studio {self.name} located at {self.address}, {self.location}.'
->>>>>>> 00f0b779dcf64491cb00d419fab9a8fafeafe77e
+        """Returns a queryset of the classes that have yet to start."""
+        return FitnessClass.objects.get_queryset(studio=self.id, startTime__gt=timezone.now())
+
