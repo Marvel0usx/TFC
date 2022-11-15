6cb569b401ed4e6dae21203f77417a7267a4a9c2
Harry Wang <18752376+Marvel0usx@users.noreply.github.com>
2022-11-15 10:18:32 -0500

    Skeleton of view classes.

--- PB/payment/views.py
+++ PB/payment/views.py
@@ -3,41 +3,68 @@ from django.views.generic import FormView
 from django.contrib.auth.models import User
 from django.http import HttpResponse, HttpResponseRedirect, JsonResponse
 from django.views.generic import FormView, DetailView, ListView
+from rest_framework.generics import UpdateAPIView, CreateAPIView, RetrieveAPIView, ListAPIView
+
 from .models import CardInfo, Payment
 from .forms import CardInfoUpdateForm
-from django.template.response import TemplateResponse
+from rest_framework.authentication import SessionAuthentication, BasicAuthentication, TokenAuthentication
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from rest_framework import generics
 
 
-class CardInfoUpdate(FormView):
-    # (TODO:haoyan)
-    pass
+class CardInfoGet(APIView):
+    authentication_classes = [TokenAuthentication]
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request):
+        pass
 
 
-def delete_card_info(request):
-    # (TODO:haoyan)
-    pass
+class PaymentHistoryView(APIView):
+    authentication_classes = [TokenAuthentication]
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request):
+        pass
 
 
-def get_card_info(request):
-    # (TODO:haoyan)
-    pass
+class PaymentFutureView(APIView):
+    authentication_classes = [TokenAuthentication]
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request):
+        pass
 
 
-class PaymentHistoryView(DetailView):
-    # (TODO:haoyan)
-    pass
+class SubscriptionPlansGet(APIView):
+    authentication_classes = [TokenAuthentication]
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request):
+        pass
 
 
-class PaymentFutureView(ListView):
-    # (TODO:haoyan)
-    pass
+class SubscriptionHistoryGet(APIView):
+    authentication_classes = [TokenAuthentication]
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request):
+        pass
 
 
-def get_subscription(request):
-    # (TODO:haoyan)
-    pass
+class CardInfoCreate(CreateAPIView):
+    authentication_classes = [TokenAuthentication]
+    permission_classes = [IsAuthenticated]
+
+    def post(self, request, *args, **kwargs):
+        pass
 
 
-class SubscriptionUpdate(FormView):
-    # (TODO:haoyan)
-    pass
+class CardInfoUpdate(UpdateAPIView):
+    authentication_classes = [TokenAuthentication]
+    permission_classes = [IsAuthenticated]
+
+    def patch(self, request, *args, **kwargs):
+        pass
