905ceca13e1809357b962d427f87c58fd55b1039
Harry Wang <18752376+Marvel0usx@users.noreply.github.com>
2022-11-15 09:07:49 -0500

    Updated admin panel.

--- PB/studios/admin.py
+++ PB/studios/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models.studio import Studio
+from .models.fitnessClass import FitnessClass
+from .models.amenity import Amenity
 
 # Register your models here.
+admin.site.register(Studio)
+admin.site.register(FitnessClass)
+admin.site.register(Amenity)
