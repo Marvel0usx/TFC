f3a1a79538af5de6c15efd8423bd0e2a10bcdb36
Harry Wang <18752376+Marvel0usx@users.noreply.github.com>
2022-11-16 09:37:06 -0500

    Updated payment url.

--- PB/payment/urls.py
+++ PB/payment/urls.py
@@ -1,6 +1,17 @@
 from django.urls import path
+from .views import *
 
-app_name = 'payment'
+app_name = "payment"
 
 urlpatterns = [
+    path("card/view/", CardInfoView.as_view()),
+    path("card/add/", CardInfoView.as_view()),
+    path("card/edit/", CardInfoView.as_view()),
+    path("subscription/plans/all/", SubscriptionPlansView.as_view()),
+    path("subscription/view/", SubscriptionsView.as_view()),
+    path("subscription/subscribe/", SubscriptionsView.as_view()),
+    path("subscription/edit/", SubscriptionsView.as_view()),
+    path("subscription/cancel/", SubscriptionsView.as_view()),
+    path("payment/upcoming/", PaymentFutureView.as_view()),
+    path("payment/history/", PaymentHistoryView.as_view()),
 ]
