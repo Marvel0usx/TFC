7b236f1ac46cbff9c44ad552e2a210971246b66a
yitaoli97 <yitaoli97@gmail.com>
2022-11-17 20:34:49 -0500

    test

--- PB/payment/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import SubscriptionPlans, Subscriptions, Payment, CardInfo
-
-# Register your models here.
-admin.site.register(SubscriptionPlans)
-admin.site.register(Subscriptions)
-admin.site.register(Payment)
-admin.site.register(CardInfo)
