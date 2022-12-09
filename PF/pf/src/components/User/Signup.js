b632ed9b2e5a1894c2c39c7e29a0235df0a2a531
tantian4 <tian.tan@mail.utoronto.ca>
2022-12-09 14:18:52 -0500

    minor changes

---
+++ PF/pf/src/components/User/Signup.js
@@ -0,0 +1,64 @@
+import React, { useState, useEffect, useContext } from 'react'
+import { useParams, Link } from 'react-router-dom';
+import { LocationContext } from '../../contexts/LocationContext';
+import Button from '../Button';
+import Input from "../Input/Input"
+
+const Signup = () => {
+    const URL = ''
+    const [query, setQuery] = useState({
+        username: "",
+        password: "",
+        password2: "",
+        email: "",
+        first_name: "",
+        last_name: "",
+        avatar: "",
+        phone_number: ""})
+        const [accounts, setAccounts] = useState([])
+        const [validate, setValidate] = useState(0)
+
+    useEffect( () => {
+        POST(`http://localhost:8000/accounts/register`)
+            .then(response=>response.json())
+            .then(data => setStudios(data.results))
+            .then(console.log(accounts))
+    }, [validate])
+
+    const register = () => setValidate(validate + 1)
+
+    return (<>
+        <h2>Sign Up</h2>
+        <div>
+            <Input title="Username" value={query.username} update={(value)=>setQuery({...query, username: value})} />
+        </div>
+        <div>
+            <Input title="Password" value={query.password} update={(value)=>setQuery({...query, password: value})} />
+        </div>
+        <div>
+            <Input title="Confirm Password" value={query.password2} update={(value)=>setQuery({...query, password2: value})} />
+        </div>
+        <div>
+            <Input title="Email" value={query.email} update={(value)=>setQuery({...query, email: value})} />
+        </div>
+        <div>
+            <Input title="First Name" value={query.first_name} update={(value)=>setQuery({...query, first_name: value})} />
+        </div>
+        <div>
+            <Input title="Last Name" value={query.last_name} update={(value)=>setQuery({...query, last_name: value})} />
+        </div>
+        <div>
+            <Input title="Avatar" type="file" value={query.avatar} update={(value)=>setQuery({...query, last_name: value})} />
+        </div>
+        <div>
+            <Input title="Phone Number" value={query.phone_number} update={(value)=>setQuery({...query, phone_number: value})} />
+        </div>
+        <div>
+            <Button label='Register' onClick={register}/>
+        </div>
+
+        </>)
+
+}
+
+export default Signup
