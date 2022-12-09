84fa14f205ca797e37d7ed94b869e44c3bf15931
tantian4 <tian.tan@mail.utoronto.ca>
2022-12-09 14:18:38 -0500

    minor changes

--- PF/pf/src/components/UI/Navbar.js
+++ PF/pf/src/components/UI/Navbar.js
@@ -6,6 +6,7 @@ const Navbar = () => {
     <nav>
         <div><Link to="/studios">Studios</Link></div>
         <div><Link to="/studios/class">Classes</Link></div>
+        <div><Link to="/accounts/register">Accounts</Link></div>
     </nav>
     <Outlet/ >
     </>)
