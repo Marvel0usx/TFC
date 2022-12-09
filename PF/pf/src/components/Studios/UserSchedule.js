6c67dd80c59f0a14dfa6acfcf767237da3c020f0
yitaoli97 <yitaoli97@gmail.com>
2022-12-08 21:04:03 -0500

    updated authorization placeholder code

--- PF/pf/src/components/Studios/UserSchedule.js
+++ PF/pf/src/components/Studios/UserSchedule.js
@@ -1,24 +1,25 @@
 
-import { useState, useEffect } from 'react'
+import { useEffect, useContext } from 'react'
+import { UserClassesContext } from '../../contexts/ClassesContext'
 import GetClasses from "./GetClasses"
  
 const Schedule = () => {
-    const [fitnessClasses, setFitnessClasses] = useState([])
+    const { userClasses, setUserClasses } = useContext(UserClassesContext)
 
     useEffect( () => {
         fetch(`http://localhost:8000/studios/class/schedule`, {
             method: 'get',
             headers: {
-                "Authorization": "Bearer token here",
+                "Authorization": `Bearer ${"tokenhere"}`,
             }
         })
         .then(response=>response.json())
-        .then(data => setFitnessClasses(data.results))
+        .then(data => setUserClasses(data.results))
     }, [])
 
     return (<>
         <h2>Upcoming Classes</h2>
-        <GetClasses fitnessClasses={fitnessClasses} />
+        <GetClasses fitnessClasses={userClasses} />
 
         </>)
     }
