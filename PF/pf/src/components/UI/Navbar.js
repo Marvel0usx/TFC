2435abea9019493aed1a7cfcbeb54a1e1c052f01
Harry Wang <18752376+Marvel0usx@users.noreply.github.com>
2022-12-10 13:45:15 -0500

    styling

--- PF/pf/src/components/UI/Navbar.js
+++ PF/pf/src/components/UI/Navbar.js
@@ -1,35 +1,50 @@
 import { useContext } from "react"
 import { Outlet, Link } from "react-router-dom"
 import { TokenContext } from "../../contexts/TokenContext"
+import M from 'materialize-css'
 
 const Navbar = () => {
     const { token } = useContext(TokenContext)
     
     if (!token) {
     return (<>
+        <div className={"nav-wrapper"}>
         <nav>
-            <Link to="/home"> Home</Link> |
-            <Link to="/register"> Register</Link> |
-            <Link to="/studios"> Studios</Link> |
-            <Link to="/studios/class"> Classes</Link> |
-            <Link to="/subscription/plans/all">Subscription Plans</Link> |
-            <Link to="/login"> Log in</Link>
-        </nav>
+            <ul className={"left hide-on-med-and-down"}>
+                <li><Link to="/home"> Home</Link></li>
+                <li><Link to="/register"> Register</Link></li>
+                <li><Link to="/studios"> Studios</Link></li>
+                <li><Link to="/studios/class"> Classes</Link></li>
+                <li><Link to="/subscription/plans/all">Subscription Plans</Link></li>
+                <li><Link to="/login"> Log in</Link></li>
+            </ul>
+        </nav></div>
         <Outlet/ >
         </>)
     }
-    return (<>
+    return (<>      
+    <ul id="dropdown1" className={"dropdown-content"}>
+        <li><Link to="/account"> My Account</Link></li>
+        <li><Link to="/payment/cardinfo/view">Credit Card</Link></li>
+    </ul>
+    <ul id="dropdown2" className={"dropdown-content"}>
+        <li><Link to="/studios"> Studios</Link></li>
+        <li><Link to="/studios/class"> Classes</Link></li>
+        <li><Link to="/schedule"> Schedule</Link></li>
+        <li class="divider"></li>
+        <li><Link to="/history"> History</Link></li>
+    </ul>       
     <nav>
-        <Link to="/home"> Home</Link> |
-        <Link to="/account"> My Account</Link> |
-        <Link to="/studios"> Studios</Link> |
-        <Link to="/studios/class"> Classes</Link> |
-        <Link to="/schedule"> Schedule</Link> |
-        <Link to="/history"> History</Link> |
-        <Link to="/subscription/plans/current">My Subscription</Link> |
-        <Link to="/payment/history">My Payment</Link> |
-        <Link to="/payment/cardinfo/view">Credit Card</Link> |
-        <Link to="/logout"> Log out</Link>
+        <div className={"nav-wrapper"}>
+            <ul className={"left hide-on-med-and-down"}>
+                <li><Link to="/home"> Home</Link></li>
+                <li><a className={"dropdown-trigger"} href="#!" dataTarget={"dropdown1"}>Account & Credit Card<i  className={"material-icons right"}>arrow_drop_down</i></a></li>
+                <li><a className={"dropdown-trigger"} href="#!" dataTarget={"dropdown2"}>Studios & Classes<i className={"material-icons right"}>arrow_drop_down</i></a></li>
+                <li><Link to="/subscription/plans/current">My Subscription</Link></li>
+                <li><Link to="/payment/history">My Payment</Link></li>
+                <li><Link to="/logout"> Log out</Link></li>
+            </ul>
+        </div>
     </nav>
     <Outlet/ >
     </>)
