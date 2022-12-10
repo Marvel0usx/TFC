1bda543d96c44f15a4c2dcdf7250200d6b98d436
Harry Wang <18752376+Marvel0usx@users.noreply.github.com>
2022-12-10 17:19:52 -0500

    Icon on navbar

--- PF/pf/src/components/UI/Navbar.js
+++ PF/pf/src/components/UI/Navbar.js
@@ -26,26 +26,29 @@ const Navbar = () => {
 
     useEffect(() => {
       M.Dropdown.init(dropDownTriggerRef1.current);
-    }, [])
+    }, [token])
 
     useEffect(() => {
       M.Dropdown.init(dropDownTriggerRef2.current);
-    }, [])
+    }, [token])
 
     if (token) {
         return (<>
+            <Link to="/home"><img src="https://www.cs.toronto.edu/~kianoosh/courses/csc309/resources/images/tfc.png" alt="TFC Logo"
+            style={{width:"5em", position:"absolute", left:"0", top:"0.9em"}}></img></Link>
             <nav>
                 <div className={"nav-wrapper"}>
+
                     <ul className={"right hide-on-med-and-down"}>
                         <li><Link to="/home"> Home</Link></li>
                         <li><a id="nav1" className={"dropdown-trigger"} href="#!" data-target="dropdown1" ref={dropDownTriggerRef1}>Account & Credit Card<i className={"material-icons right"}>arrow_drop_down</i></a>
-                            <ul id="dropdown1" className={"dropdown-content"} style={{position: "sticky", top: 0, zIndex:100}}>
+                            <ul id="dropdown1" className={"dropdown-content"} style={{position: "sticky", zIndex:100}}>
                                 <li><Link to="/account"> My Account</Link></li>
                                 <li><Link to="/payment/cardinfo/view">Credit Card</Link></li>
                             </ul>
                         </li>
                         <li><a id="nav2" className={"dropdown-trigger"} href="#!" data-target="dropdown2" ref={dropDownTriggerRef2}>Studios & Classes<i className={"material-icons right"}>arrow_drop_down</i></a>
-                            <ul id="dropdown2" className={"dropdown-content"} style={{position: "sticky", top: 0, zIndex:100}}>
+                            <ul id="dropdown2" className={"dropdown-content"} style={{position: "sticky", zIndex:100}}>
                                 <li><Link to="/studios"> Studios</Link></li>
                                 <li><Link to="/studios/class"> Classes</Link></li>
                                 <li><Link to="/schedule"> Schedule</Link></li>
@@ -64,9 +67,12 @@ const Navbar = () => {
     }
     else {
     return (<>
+        <Link to="/home"><img src="https://www.cs.toronto.edu/~kianoosh/courses/csc309/resources/images/tfc.png" alt="TFC Logo" 
+            style={{width:"5em", position:"absolute", left:"0", top:"0.9em"}}>
+        </img></Link>
         <div className={"nav-wrapper"}>
             <nav>
-                <ul className={"left hide-on-med-and-down"}>
+                <ul className={"right hide-on-med-and-down"}>
                     <li><Link to="/home"> Home</Link></li>
                     <li><Link to="/register"> Register</Link></li>
                     <li><Link to="/studios"> Studios</Link></li>
