71b5c93ae38897a6af4809fd841b122378f32281
yitaoli97 <yitaoli97@gmail.com>
2022-12-10 10:18:44 -0500

    created tokencontext.js and added to app.js

--- PF/pf/src/App.js
+++ PF/pf/src/App.js
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import Router  from "./components/Router/Router";
 import { LocationContext } from "./contexts/LocationContext";
-
+import { TokenContext } from "./contexts/TokenContext";
 import { UserClassesContext } from "./contexts/ClassesContext";
 import { SubscriptionContext } from "./contexts/SubscriptionContext";
 
@@ -9,8 +9,10 @@ function App() {
   const [location, setLocation] = useState({x:"", y:""})
   const [userClasses, setUserClasses] = useState([])
   const [subCxt] = useState({subid:undefined})
+  const [token, setToken] = useState(null)
 
   return (<>
+  <TokenContext.Provider value={{token, setToken}}>
     <UserClassesContext.Provider value={{userClasses, setUserClasses}}>
       <LocationContext.Provider value={{location, setLocation}}>
         <SubscriptionContext.Provider value={{subCxt}}>
@@ -18,6 +20,7 @@ function App() {
         </SubscriptionContext.Provider>
       </LocationContext.Provider>
     </UserClassesContext.Provider>
+  </TokenContext.Provider>
   </>);
 }
 
