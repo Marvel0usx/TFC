56b2128069061633bcebb729d4578aa44a7d2d39
yitaoli97 <yitaoli97@gmail.com>
2022-12-10 13:02:29 -0500

    added to home page

--- PF/pf/src/components/Home/Home.js
+++ PF/pf/src/components/Home/Home.js
@@ -1,3 +1,10 @@
 export const Home = () => {
-    return <h1>Toronto Fitness Club</h1>
+    return <><h1>Toronto Fitness Club</h1>
+    <img src="https://www.cs.toronto.edu/~kianoosh/courses/csc309/resources/images/tfc.png" alt="TFC Logo"></img>
+    <p>Welcome to the Toronto Fitness Club. Our mission is to provide you with the best possible services with our
+        myriads of modern studios and classes offered by professional coaches and trainers. Feel free to take a look
+        at our studios and classes and register an account to enroll in our classes.
+    </p>
+    <img src="https://images.adsttc.com/media/images/54b7/4f72/e58e/cea3/b400/0084/large_jpg/portada_003_North_Exterior_Dawn.jpg?1421299558" alt="Studio"></img>
+    </>
 }
