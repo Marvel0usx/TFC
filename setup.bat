a1b0fd10551218bacdd04459c3f9ac84a314ccb8
HaoyanWang <1071741427@qq.com>
2022-12-10 19:47:24 -0500

    Update setup.bat

--- setup.bat
+++ setup.bat
@@ -8,14 +8,16 @@ call .\309project\Scripts\activate.bat
 echo Install Django...
 pip install django==4.1.2 djangorestframework Pillow django-filter djangorestframework-simplejwt
 pip install django-cors-headers
-cd PB
+cd .\PB
 echo Make migrations
 python manage.py makemigrations
 python manage.py migrate --run-syncdb
 
 cd ..
-cd PF
-cd pf
-npm build
+cd .\PF
+cd .\pf
+npm install react-router-dom
+npm install materialize-css@next 
+npm run build
 
 pause
